d2eb3672944b557fea1e2a6b75ecefd3003f35b0
James <jafisher32@gmail.com>
2025-02-09 15:09:27 -0500

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/meret-and-james/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  base: '/meret-and-james/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    // Ensure assets are built with the correct base path
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      }
     }
   },
   server: {
