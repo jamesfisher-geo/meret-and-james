f73bfab5f7c4ea4f9f3b6863809e5af279ecfcdd
James <jafisher32@gmail.com>
2025-02-09 14:45:12 -0500

    revert

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/meret-and-james/',
   plugins: [
@@ -14,15 +16,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    // external: ['vue'],
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
   }
 })
